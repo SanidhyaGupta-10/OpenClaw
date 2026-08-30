a205314868f851e4c5b5da6f2b64b57169433b6e
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-08-30 22:00:39 +0530

    feat: initialize express server with environment configuration and body parsing middleware

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import "dotenv/config"
+import express, { urlencoded } from "express";
+
+const app = express();
+const PORT = process.env.PORT
+
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
