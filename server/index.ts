1aee37d36e0eb7bc87f349a1306ccfcef292df92
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-08-31 22:13:04 +0530

    chore: install and sync node_modules dependencies

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,9 @@ const PORT = process.env.PORT
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+    res.send("Server is running");
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
