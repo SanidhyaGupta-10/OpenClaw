a1dfecf02d02e487d06e6e8204d6a2def352ac3b
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-09-08 20:42:45 +0530

    deleted this project

--- server/app.ts
+++
@@ -1,9 +0,0 @@
-import http from "http";
-import app from "./index";
-
-const server = http.createServer(app);
-const PORT = process.env.PORT;
-
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
