fd4ce0ed321ef5193f39d14083594051e0792924
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-09-17 22:21:36 +0530

    feat: initialize CLI application setup using commander

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import { Command } from "commander";
+
+const program = new Command();
+
+// Gonna Start developing this...
+// and it will be a cli application which can be used to automate web-related tasks.
